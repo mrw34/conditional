58fa17889680fa23af13911f9d64c6658e5b4ac4
mrw34 <mark.woodbridge@cantab.net>
2013-05-26 06:56:48 -0700

    Latest

--- conditional.js
+++ conditional.js
@@ -1,5 +1,4 @@
 if (Meteor.isClient) {
-
   Template.masthead.helpers({
     diseaseIs: function(disease) {
       return Session.equals('disease', disease);
@@ -7,23 +6,37 @@ if (Meteor.isClient) {
   });
 
   Template.masthead.events({
-    'click a': function(e) {
+    'click li > a': function(e) {
       e.preventDefault();
-      Session.set('disease', e.target.href.split('#')[1]);
+      var disease = e.target.href.split('#')[1];
+      Meteor.users.update(Meteor.userId(), {$set: {'profile.disease': disease}});
+      Session.set('disease', disease);
     },
-    'click h3': function(e) {
+    'click div > a': function(e) {
       e.preventDefault();
+      Meteor.users.update(Meteor.userId(), {$unset: {'profile.disease': true}});
       Session.set('disease', null);
     }
   });
+  https://dl.dropboxusercontent.com/u/1120779/NHSHackDay/measles.pdf
 
   Handlebars.registerHelper('disease', function(disease) {
     return Session.get('disease');
+  });
+
+  Meteor.startup(function() {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log(position.coords.latitude);
+      console.log(position.coords.longitude);
+      Meteor.users.update(Meteor.userId(), {$set: {'profile.coords': position.coords}});
+    });
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    var result = Meteor.http.get('http://search.twitter.com/search.json', {params: {q: 'measles', geocode: '51.5171,0.1062,1mi'}});
+    console.log(result);
   });
 }
